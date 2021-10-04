20608790f487624f4782dc214c6036c424f180ee
Milesfeng <milesfeng18>
2021-10-04 14:55:11 +0800

    add Param

--- go-hello-world/hello_world.go
+++ go-hello-world/hello_world.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -12,8 +11,10 @@ func main() {
 	e := echo.New()
 	// add middleware and routes
 	// ...
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, fmt.Sprintf("<p><h2>Hello World  !! </h2></p>"))
+	e.GET("/:id", func(c echo.Context) error {
+		// return c.HTML(http.StatusOK, fmt.Sprintf("<p><h2>Hello World  !! </h2></p>"))
+		id := c.Param("id")
+		return c.String(http.StatusOK, "Hello "+id)
 	})
 
 	if err := e.Start(":8080"); err != http.ErrServerClosed {

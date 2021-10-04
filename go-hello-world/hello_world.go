0b3d3cb0747035643a1cf853322e59324ff1e3b8
Milesfeng <milesfeng18>
2021-10-04 14:45:55 +0800

    test

--- go-hello-world/hello_world.go
+++ go-hello-world/hello_world.go
@@ -13,7 +13,7 @@ func main() {
 	// add middleware and routes
 	// ...
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, fmt.Sprintf("<p><h2>Hello World !!!!!!!</h2></p>"))
+		return c.HTML(http.StatusOK, fmt.Sprintf("<p><h2>Hello World  !! </h2></p>"))
 	})
 
 	if err := e.Start(":8080"); err != http.ErrServerClosed {

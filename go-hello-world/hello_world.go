f3cc086ff8fea7caa735364d04cbddada16a67ee
Milesfeng <milesfeng18>
2021-10-04 17:21:49 +0800

    add v2

--- go-hello-world/hello_world.go
+++ go-hello-world/hello_world.go
@@ -14,7 +14,7 @@ func main() {
 	e.GET("/:id", func(c echo.Context) error {
 		// return c.HTML(http.StatusOK, fmt.Sprintf("<p><h2>Hello World  !! </h2></p>"))
 		id := c.Param("id")
-		return c.String(http.StatusOK, "Hi V1"+id)
+		return c.String(http.StatusOK, "Hi V2 "+id)
 	})
 
 	if err := e.Start(":8080"); err != http.ErrServerClosed {
